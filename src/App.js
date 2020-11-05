56536f7e2529429cb5c6afd614ec019baa2e35e1
JIHO KIM <getz213@naer.com>
2020-11-05 18:26:21 +0900

    06-03 깃허브에 리액트 앱 업로드 하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  }
+
+  getMovies = async () => {
+    // 구조 할당을 해서 받는 것이 좋다 
+    const {
+      data : {
+        data : { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    //console.log(movies);
+
+    this.setState({movies, isLoading: false}); // ES6에서 객체 키와 변수명이 동일하면 축약이 가능하다 
+
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    
+    this.getMovies();
   }
 
   render(){
